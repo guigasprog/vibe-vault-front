38f734d14860b1094858637c3cac57fddf4db160
Guilherme Delgado Martins <2311600261@fema.edu.br>
2024-11-06 13:08:10 -0300

    fix: arrumar tela de login para que possa por o register page

---
+++ src/app/components/dialog.component.ts
@@ -0,0 +1,78 @@
+import { NgClass, NgIf } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'vv-dialog',
+  standalone: true,
+  imports: [NgClass, NgIf],
+  template: `
+    <div class="dialog-backdrop" *ngIf="visible" (click)="closeModal()"></div>
+    <div class="dialog" *ngIf="visible">
+      <div class="dialog-header">
+        <h3 class="title">{{ header }}</h3>
+        <span class="dialog-close" (click)="closeModal()">&times;</span>
+      </div>
+      <div class="dialog-content content-text">
+        <ng-content></ng-content>
+      </div>
+      <div class="dialog-footer">
+        <ng-content select="[footer]"></ng-content>
+      </div>
+    </div>
+  `,
+  styles: [
+    `
+      .dialog-backdrop {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        background: rgba(0, 0, 0, 0.4);
+      }
+      .dialog {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 90%;
+        max-width: 500px;
+        background: #3a3a3a;
+        border: 0;
+        border-radius: 8px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+      }
+      .dialog-header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 24px 24px 12px 24px;
+        background: #3a3a3a;
+        border-radius: 5px 5px 0 0;
+      }
+      .dialog-content {
+        padding: 12px 24px 0px 24px;
+      }
+      .dialog-footer {
+        padding: 0px 24px 24px 24px;
+        display: flex;
+        background: #3a3a3a;
+        border-radius: 0 0 5px 5px;
+      }
+      .dialog-close {
+        scale: 1.7;
+        cursor: pointer;
+      }
+    `,
+  ],
+})
+export class DialogComponent {
+  @Input() header: string = '';
+  @Input() visible: boolean = false;
+  @Output() visibleChange = new EventEmitter<boolean>();
+
+  closeModal() {
+    this.visible = false;
+    this.visibleChange.emit(this.visible);
+  }
+}
--- src/app/representation/pages/login/login.component.ts
+++ src/app/representation/pages/login/login.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { DialogComponent } from '../../../components/dialog.component';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [DialogComponent, NgStyle],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  isDialogVisible: boolean = false;
 
+  reg: boolean = false;
+
+  windowWidth: number = window.innerWidth;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.windowWidth = window.innerWidth;
+  }
+
+  registerPageVisible() {
+    this.reg = !this.reg;
+  }
+
+  openDialog() {
+    this.isDialogVisible = true;
+  }
+
+  closeDialog() {
+    this.isDialogVisible = false;
+  }
 }
